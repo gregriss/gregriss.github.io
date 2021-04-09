66c3bcaa4912fbed2d0ff9617a4b73c3ec2e83b1
Greg Riss <gregriss23@gmail.com>
2021-04-08 21:49:03 -0400

    commenting out js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,16 +1,16 @@
-let nameInput = document.getElementById('nameInput');
-let emailInput = document.getElementById('emailInput');
-let messageInput = document.getElementById('userMessage');
-let submitBtn = document.getElementById('contact-submit-btn');
+// let nameInput = document.getElementById('nameInput');
+// let emailInput = document.getElementById('emailInput');
+// let messageInput = document.getElementById('userMessage');
+// let submitBtn = document.getElementById('contact-submit-btn');
 
 
-submitBtn.addEventListener('click', (event) => {
+// submitBtn.addEventListener('click', (event) => {
     
-    event.preventDefault();
-    const user = {
-        name : nameInput.value,
-        email : emailInput.value,
-        message : messageInput.value
-    }
-    console.log(user);
-})
+//     // event.preventDefault();
+//     const user = {
+//         name : nameInput.value,
+//         email : emailInput.value,
+//         message : messageInput.value
+//     }
+//     console.log(user);
+// })
