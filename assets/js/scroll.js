1385d91dea873983dfac83bc4e3199ed9c84df3c
Greg Riss <gregriss23@gmail.com>
2021-04-19 01:27:17 -0400

    move svg to assets/images/icons folder, edit color scheme

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -2,12 +2,10 @@ document.addEventListener("DOMContentLoaded", function(){
     window.addEventListener('scroll', function() {
         if (window.scrollY > 0) {
             document.getElementById('header').classList.remove('top-nav');
-            document.getElementById('header').classList.add('fixed-top');
-            document.getElementById('header').classList.add('scroll-nav');
+            document.getElementById('header').classList.add('fixed-top', 'scroll-nav');
         } else {
-            document.getElementById('header').classList.remove('fixed-top');
+            document.getElementById('header').classList.remove('fixed-top', 'scroll-nav');
             document.getElementById('header').classList.add('top-nav');
-            document.getElementById('header').classList.remove('scroll-nav');
         }
     })
 });
