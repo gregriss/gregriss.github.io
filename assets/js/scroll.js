4e40e6e5455104d2f6044f5b690b4e276f4899fc
Greg Riss <gregriss23@gmail.com>
2021-04-19 02:42:21 -0400

    add sticky footer, restyle footer svg sizes, add phone modal

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -9,3 +9,33 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     })
 });
+
+let phone = document.querySelector('#phone');
+let modal = document.querySelector('#phone-modal');
+phone.addEventListener('click', function(){
+    modal.classList.toggle('hide');
+})
+//     function initLazySections() {
+//         // var sectionsToLoad = document.querySelectorAll('img');
+        
+//         if('IntersectionObserver' in window) {
+//           const observer = new IntersectionObserver((items, observer) => {
+//             items.forEach((item) => {
+//               if(item.isIntersecting) {
+//                 console.log(item.target);
+//                 var src = item.target.setAttribute("display", "visible");
+//                 item.target.setAttribute("src", src);
+//                 // loadImages(item.target);
+//                 observer.unobserve(item.target);
+//               }
+//             });
+//           });
+//           sectionsToLoad.forEach((section) => {
+//             observer.observe(section);
+//           });
+//         } else {
+//           sectionsToLoad.forEach((section) => {
+//             // loadImages(img);
+//           });
+//         }
+//       }
